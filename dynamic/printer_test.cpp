c613e79a018b11cd4fd6359659bfe5b042ca8bbc
Jeuk Kim <gee6809@gmail.com>
2022-02-15 13:35:41 +0900

    [Printer] print NONE for empty employees list

--- dynamic/printer_test.cpp
+++ dynamic/printer_test.cpp
@@ -5,3 +5,18 @@
 TEST(Printer, NullTest) {
 	EXPECT_EQ(Printer::getPrintString(ParserResult(), std::vector<EmployeeInfo>()), "");
 }
+
+TEST(Printer, EmptyTest) {
+	ParserResult empty_result;
+	empty_result.operationType = OPERATION_TYPE::ADD;
+	EXPECT_EQ(Printer::getPrintString(empty_result, std::vector<EmployeeInfo>()), "");
+
+	empty_result.operationType = OPERATION_TYPE::DEL;
+	EXPECT_EQ(Printer::getPrintString(empty_result, std::vector<EmployeeInfo>()), "DEL,NONE");
+
+	empty_result.operationType = OPERATION_TYPE::SCH;
+	EXPECT_EQ(Printer::getPrintString(empty_result, std::vector<EmployeeInfo>()), "SCH,NONE");
+
+	empty_result.operationType = OPERATION_TYPE::MOD;
+	EXPECT_EQ(Printer::getPrintString(empty_result, std::vector<EmployeeInfo>()), "MOD,NONE");
+}
