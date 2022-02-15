7cc1d47a0365bc178f7a0d80e6a98e8047a757af
Jeuk Kim <gee6809@gmail.com>
2022-02-15 13:35:41 +0900

    [Printer] print NONE for empty employees list

--- dynamic/printer.cpp
+++ dynamic/printer.cpp
@@ -2,5 +2,24 @@
 
 std::string Printer::getPrintString(const ParserResult& parserResult, const std::vector<EmployeeInfo>& targetEmployees)
 {
-	return std::string();
+	std::string result;
+	if (parserResult.operationType == OPERATION_TYPE::DEL) {
+		result += "DEL,";
+	}
+	else if (parserResult.operationType == OPERATION_TYPE::SCH) {
+		result += "SCH,";
+	}
+	else if (parserResult.operationType == OPERATION_TYPE::MOD) {
+		result += "MOD,";
+	}
+	else {
+		return std::string();
+	}
+
+	if (targetEmployees.empty()) {
+		result += "NONE";
+		return result;
+	}
+
+	return result;
 }
