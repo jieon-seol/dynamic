916cb11e6fd0ff5e55d00ccb9857fc6f05d8b4ad
sky64422 <69107889+sky64422@users.noreply.github.com>
2022-02-16 08:23:48 +0900

    [Searcher] Factory Class 생성 1. TC 작성 2. TC Fail 3. Class, 메소드 구현 4. TC Pass 1~4 반복하며 구현

--- dynamic/Searcher.h
+++ dynamic/Searcher.h
@@ -1,53 +1,100 @@
 #pragma once
-#include <iostream>
 #include <vector>
 #include "Define.h"
 
-using namespace std;
-
 class Searcher {
 public:
-	Searcher(vector<EmployeeInfo>* pDb) : pDb_(pDb) {}
-	virtual vector<EmployeeInfo> search(const ParserResult& parserResult) = 0;
+	Searcher(std::vector<EmployeeInfo>* pDataBase) : pDataBase_(pDataBase) {}
+	virtual std::vector<EmployeeInfo> search(const ParserResult& parserResult) const = 0;
+
 protected:
-	vector<EmployeeInfo>* pDb_;
+	std::vector<EmployeeInfo>* pDataBase_;
 };
 
 class EmployeeNumSearcher : public Searcher {
 public:
-	EmployeeNumSearcher(vector<EmployeeInfo>* pDb) : Searcher(pDb) {}
-	virtual vector<EmployeeInfo> search(const ParserResult& parserResult) override;
+	EmployeeNumSearcher(std::vector<EmployeeInfo>* pDataBase) : Searcher(pDataBase) {}
+
+private:
+	virtual std::vector<EmployeeInfo> search(const ParserResult& parserResult) const override;
 };
 
 class NameSearcher : public Searcher {
 public:
-	NameSearcher(vector<EmployeeInfo>* pDb) : Searcher(pDb) {}
-	string getNameByOption(const string& name, const OPTION2 option);
-	virtual vector<EmployeeInfo> search(const ParserResult& parserResult) override;
+	NameSearcher(std::vector<EmployeeInfo>* pDataBase) : Searcher(pDataBase) {}
+
+private:
+	virtual std::vector<EmployeeInfo> search(const ParserResult& parserResult) const override;
+	std::string filterData(const std::string& name, const OPTION2 option) const;
 };
 
 class ClSearcher : public Searcher {
 public:
-	ClSearcher(vector<EmployeeInfo>* pDb) : Searcher(pDb) {}
-	virtual vector<EmployeeInfo> search(const ParserResult& parserResult) override;
+	ClSearcher(std::vector<EmployeeInfo>* pDataBase) : Searcher(pDataBase) {}
+
+private:
+	virtual std::vector<EmployeeInfo> search(const ParserResult& parserResult) const override;
 };
 
 class PhoneNumberSearcher : public Searcher {
 public:
-	PhoneNumberSearcher(vector<EmployeeInfo>* pDb) : Searcher(pDb) {}
-	string getNumberByOption(const string& phoneNumber, const OPTION2 numberOption);
-	virtual vector<EmployeeInfo> search(const ParserResult& parserResult) override;
+	PhoneNumberSearcher(std::vector<EmployeeInfo>* pDataBase) : Searcher(pDataBase) {}
+
+private:
+	virtual std::vector<EmployeeInfo> search(const ParserResult& parserResult) const override;
+	std::string filterData(const std::string& phoneNumber, const OPTION2 numberOption) const;
 };
 
 class BirthdaySearcher : public Searcher {
 public:
-	BirthdaySearcher(vector<EmployeeInfo>* pDb) : Searcher(pDb) {}
-	string getBirthDayByOption(const string& birthDay, const OPTION2 birthOption);
-	virtual vector<EmployeeInfo> search(const ParserResult& parserResult) override;
+	BirthdaySearcher(std::vector<EmployeeInfo>* pDataBase) : Searcher(pDataBase) {}
+
+private:
+	virtual std::vector<EmployeeInfo> search(const ParserResult& parserResult) const override;
+	std::string filterData(const std::string& birthDay, const OPTION2 birthOption) const;
 };
 
 class CertiSearcher : public Searcher {
 public:
-	CertiSearcher(vector<EmployeeInfo>* pDb) : Searcher(pDb) {}
-	virtual vector<EmployeeInfo> search(const ParserResult& parserResult) override;
+	CertiSearcher(std::vector<EmployeeInfo>* pDataBase) : Searcher(pDataBase) {}
+
+private:
+	virtual std::vector<EmployeeInfo> search(const ParserResult& parserResult) const override;
+};
+
+class IFactorySearcher {
+public:
+	virtual ~IFactorySearcher() {}
+	virtual Searcher* getSearcher(const ParserResult& parserResult) const = 0;
+};
+
+class FactorySearcher : public IFactorySearcher {
+public:
+	FactorySearcher(std::vector<EmployeeInfo>* pDataBase) {
+		pEmployeeNumSearcher_ = new EmployeeNumSearcher(pDataBase);
+		pNameSearcher_ = new NameSearcher(pDataBase);
+		pClSearcher_ = new ClSearcher(pDataBase);
+		pPhoneNumSearcher_ = new PhoneNumberSearcher(pDataBase);
+		pBirthdaySearcher_ = new BirthdaySearcher(pDataBase);
+		pCertiSearcher_ = new CertiSearcher(pDataBase);
+	}
+	~FactorySearcher() {
+		delete pEmployeeNumSearcher_;
+		delete pNameSearcher_;
+		delete pClSearcher_;
+		delete pPhoneNumSearcher_;
+		delete pBirthdaySearcher_;
+		delete pCertiSearcher_;
+	}
+
+private:
+	virtual Searcher* getSearcher(const ParserResult& parserResult) const override;
+
+private:
+	Searcher* pEmployeeNumSearcher_;
+	Searcher* pNameSearcher_;
+	Searcher* pClSearcher_;
+	Searcher* pPhoneNumSearcher_;
+	Searcher* pBirthdaySearcher_;
+	Searcher* pCertiSearcher_;
 };
