8d90204e28cf8bfe2d6345a3b6c1e3aaf3e3399e
Jeuk Kim <gee6809@gmail.com>
2022-02-16 12:31:13 +0900

    [DBManager] BasicDBTest
    1. DBTest test fixture 구현
    2. sample input에 대한 test code 작성 (BasicDBTest)
    3. DBManager Query 함수 구현

--- dynamic/DBManager.cpp
+++ dynamic/DBManager.cpp
@@ -1,7 +1,23 @@
 #include "DBManager.h"
 #include "Printer.h"
+#include "Parser.h"
 
 std::string DBManager::Query(std::string queryString)
 {
-    return std::string();
+    // TODO: invalid한 queryString 검사추가
+    if (queryString.empty()) {
+        return std::string();
+    }
+
+    Parser parser;
+    ParserResult parseResult = parser.parse(queryString);
+    Operator* op = opFactory->getOperator(parseResult);
+    Searcher* sc = scFactory->getSearcher(parseResult);
+
+
+    std::vector<EmployeeInfo> lists = sc->search(parseResult);
+    op->operate(&lists, parseResult);
+    std::string queryResult = Printer::GetPrintString(parseResult, lists);
+
+    return queryResult;
 }
